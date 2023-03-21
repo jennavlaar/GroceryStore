92cb493337b09da5275859bf69c555db1d2670b5
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2023-03-21 13:11:05 -0600

    Added tables
    - more tables

--- SQLite/db.py
+++ SQLite/db.py
@@ -15,14 +15,100 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS groceries (
             PRIMARY KEY (item_id)
         )""")
 
-#Create users table
+#Create user table
 cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+            username text NOT NULL,
+            PRIMARY KEY (username)
+        )""")
+
+#Create registered registered user table
+cursor.execute("""CREATE TABLE IF NOT EXISTS registeredusers(
             username text NOT NULL,
             password text NOT NULL,
             name text NOT NULL,
             address text NOT NULL,
-            PRIMARY KEY (username)
+            FOREIGN KEY (username) REFERENCES users(username)
         )""")
+
+#Create admin user table
+cursor.execute("""CREATE TABLE IF NOT EXISTS admin(
+            admin_id integer NOT NULL,
+            username text NOT NULL,
+            password text NOT NULL,
+            name text NOT NULL,
+            address text NOT NULL,
+            PRIMARY KEY (admin_id),
+            FOREIGN KEY (username) REFERENCES users(username)
+        )""")
+
+#Create order table
+cursor.execute("""CREATE TABLE IF NOT EXISTS orders(
+            order_id integer NOT NULL,
+            username text NOT NULL,
+            address text NOT NULL,
+            PRIMARY KEY (order_id),
+            FOREIGN KEY (username) REFERENCES users(username)
+        )""")
+
+#Create receipt table
+cursor.execute("""CREATE TABLE IF NOT EXISTS receipt(
+            order_id integer NOT NULL,
+            total real NOT NULL,
+            PRIMARY KEY (order_id),
+            FOREIGN KEY (order_id) REFERENCES orders(order_id)
+        )""")
+
+
+
+#Create employee table
+cursor.execute("""CREATE TABLE IF NOT EXISTS employees(
+            employee_id integer NOT NULL,
+            first_name text NOT NULL,
+            last_name text NOT NULL,
+            employer text NOT NULL,
+            PRIMARY KEY (employee_id)
+            FOREIGN KEY (employer) REFERENCES supplier(name)
+        )""")
+
+#Create supplier table
+cursor.execute("""CREATE TABLE IF NOT EXISTS suppliers(
+            supplier_name text NOT NULL,
+            product text NOT NULL,
+            PRIMARY KEY (supplier_name)
+            FOREIGN KEY (product) REFERENCES product(product_name)
+        )""")
+
+#Create product table
+cursor.execute("""CREATE TABLE IF NOT EXISTS product(
+            product_name text NOT NULL,
+            stock int NOT NULL,
+            PRIMARY KEY (product_name)
+        )""")
+
+#Create product table
+cursor.execute("""CREATE TABLE IF NOT EXISTS supplies(
+            supplier text NOT NULL,
+            grocery_item text NOT NULL,
+            FOREIGN KEY (supplier) REFERENCES suppliers(supplier_name)
+            FOREIGN KEY (grocery_item) REFERENCES groceries(item_id)
+        )""")
+
+#Create farm table
+cursor.execute("""CREATE TABLE IF NOT EXISTS farm(
+            farm_name text NOT NULL,
+            location text NOT NULL,
+            PRIMARY KEY (farm_name)
+        )""")
+
+#Create sells to table
+cursor.execute("""CREATE TABLE IF NOT EXISTS sellsto(
+            farm_name text NOT NULL,
+            supplier text NOT NULL,
+            FOREIGN KEY (supplier) REFERENCES suppliers(supplier_name)
+            FOREIGN KEY (farm_name) REFERENCES farm(farm_name)
+        )""")
+
+
 
 connect.close()
 
@@ -60,7 +146,7 @@ def insert_user(username, password, name, address):
     try:
         connection = sqlite3.connect('grocery.db')
         cursor = connection.cursor()
-        insert_query = """INSERT INTO users 
+        insert_query = """INSERT INTO registeredusers 
                         (username, password, name, address) VALUES (?, ?, ?, ?)"""
         data = (username, password, name, address)
         cursor.execute(insert_query, data)
@@ -96,12 +182,12 @@ def fetch_item(item_id):
             connection.close()
             
 #fetch user from users table
-def fetch_item(username):
+def fetch_user(username):
     try:
         connection = sqlite3.connect('grocery.db')
         cursor = connection.cursor()
                 
-        fetch_query = """SELECT * from users where username = ?"""
+        fetch_query = """SELECT * from registeredusers where username = ?"""
         cursor.execute(fetch_query, (username,))
         record = cursor.fetchall()
 
@@ -122,6 +208,7 @@ def fetch_item(username):
 #test inserts/fetches
 insert_item(1,"Bread", "images/bread.png", "Food", 12, 2.97)
 fetch_item(1)
+fetch_user("john")
 insert_user("john", "pass1", "john", "123 john st")
 insert_user("john", "pass1", "john", "123 john st")
 
